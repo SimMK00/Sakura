55f1eb1ac0eef43760b1288caa5f580593d292ea
Cyneric <cyneric00@gmail.com>
2022-08-14 22:21:06 +0800

    Updated flip and avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,16 +1,48 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
+    
 module.exports = {
-    name: "avatar",
-    description: "Returns avatar of user specified",
-    execute(msg,args) {
-        let Discord = require('discord.js');
-        let embed = new Discord.MessageEmbed();
-        if (!args[1]) {
-            msg.channel.send(embed.setImage(`${msg.author.avatarURL({ size: 2048 })}`));
-        } else if (msg.mentions.users.first()) {
-            let user = msg.mentions.users.first();
-            msg.channel.send(embed.setImage(`${user.avatarURL({ size: 2048 })}`));
-        } else {
-            msg.channel.send(`Valid user please you ignorant swine`);
-        }
+    data: new SlashCommandBuilder()
+        .setName("avatar")
+        .setDescription("Returns avatar of user specified")
+        .addUserOption(option=>
+            option.setName("target")
+            .setDescription("Select a user"))
+        .addIntegerOption(option => 
+            option.setName("size")
+            .setDescription("Select size of the avatar")
+            .addChoices(
+                {name: "16",value: 16},
+                {name: "32",value: 32},
+                {name: "56",value: 56},
+                {name: "64",value: 64},
+                {name: "96",value: 96},
+                {name: "128",value: 128},
+                {name: "256",value: 256},
+                {name: "300",value: 300},
+                {name: "512",value: 512},
+                {name: "600",value: 600},
+                {name: "1024",value: 1024},
+                {name: "2048",value: 2048},
+                {name: "4096",value: 4096},
+            )),
+    /**
+     * 
+     * @param {Discord.BaseCommandInteraction} interaction 
+     */
+    async execute(interaction) {
+        const embed = new Discord.MessageEmbed();
+        const user = interaction.options.getUser("target");
+        const size = interaction.options.getInteger("size");
+        await interaction.reply({
+            embeds: [
+                embed.setAuthor({
+                    name: `${interaction.user.tag}`,
+                    iconURL: interaction.user.avatarURL()
+                }).setTitle(`Avatar of ${user.tag}`).setImage(user.avatarURL({size: size}))
+                .setColor("LUMINOUS_VIVID_PINK")
+            ]
+        })
+        
     }
 }
--- commands/flip.js
+++ commands/flip.js
@@ -1,12 +1,38 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
+
 module.exports = {
-    name: "flip",
-    description: "Flips a coin",
-    execute(msg){
-          let flipnum = Math.round(Math.random());
-          if (flipnum == 0){
-              msg.channel.send(`${msg.author.username} has flipped **head**`,{files:["./images/head.png"]})
-          } else {
-              msg.channel.send(`${msg.author.username} has flipped **tail**`,{files:["./images/tail.png"]})
-          }
-    }
+    
+    data: new SlashCommandBuilder()
+        .setName("flip")
+        .setDescription("Flips a coin"),
+    /**
+     * 
+     *  @param {Discord.BaseCommandInteraction} interaction  
+     */
+    async execute(interaction){
+        try {
+            const flipnum = Math.round(Math.random());
+            const embed = new Discord.MessageEmbed();
+
+            embed.setAuthor({
+                name: `${interaction.user.tag}`,
+                iconURL: interaction.user.avatarURL()
+            }).setColor("LUMINOUS_VIVID_PINK")
+
+            if (flipnum == 0){
+                embed.setDescription(`${interaction.user.username} has flipped **head**`).setImage("https://cdn.discordapp.com/attachments/696045581180862585/1005774664167063613/head.png")
+            } else {
+                embed.setDescription(`${interaction.user.username} has flipped **tail**`).setImage("https://cdn.discordapp.com/attachments/696045581180862585/1005776838691409950/tail.png")
+            }
+
+            await interaction.reply({
+                embeds: [
+                    embed
+                ]
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    } 
 }
