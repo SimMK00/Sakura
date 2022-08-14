d7cdbde9645de519a8cb00ed6d545f58bc933ca1
Cyneric <cyneric00@gmail.com>
2022-08-14 22:21:15 +0800

    Updated prune command

--- commands/prune.js
+++ commands/prune.js
@@ -1,14 +1,32 @@
-const { execute } = require("./ping")
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Discord = require('discord.js');
 
 module.exports = {
-    name: "prune",
-    description: "Deletes messages",
-    async execute(msg,args){
-            if(!args[1]){
-                msg.channel.send(`Please provide a valid number~`).then(d_msg =>{d_msg.delete({timeout:5000})});
-            } else {
-                await msg.channel.bulkDelete(parseInt(args[1])+1);
-                msg.channel.send(`${args[1]} messages have been deleted!`).then(d_msg =>{d_msg.delete({timeout:5000})});
-            }
-    }
+    data: new SlashCommandBuilder()
+        .setName("prune")
+        .setDescription("Bulk delete messages")
+        .addIntegerOption(option =>
+            option.setName("number")
+            .setDescription("Number of messages to delete")),
+    /**
+     * 
+     * @param {Discord.BaseCommandInteraction} interaction 
+     */
+    async execute(interaction){
+        const numDelete = interaction.options.getInteger("number");
+        const embed = new Discord.MessageEmbed();
+
+        await interaction.channel.bulkDelete(numDelete)
+        await interaction.reply({
+            embeds: [
+                embed.setAuthor({
+                    name: `${interaction.user.tag}`,
+                    iconURL: interaction.user.avatarURL()
+                }).setTitle(`Bulk delete`).setDescription(`${numDelete} messages deleted`)
+                .setColor("LUMINOUS_VIVID_PINK")
+            ]
+        }).then(setTimeout(() => {
+            interaction.deleteReply()
+        }, 2000))
+    } 
 }
