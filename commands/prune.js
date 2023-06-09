402ba72a49fb00aec10ac85b6ef5caf728380096
Cyneric <cyneric00@gmail.com>
2023-06-09 20:50:52 +0800

    updated prune command

--- commands/prune.js
+++ commands/prune.js
@@ -6,28 +6,54 @@ module.exports = {
         .setName("prune")
         .setDescription("Bulk delete messages")
         .addIntegerOption(option =>
-            option.setName("number")
-            .setRequired(true)
-            .setDescription("Number of messages to delete")),
+            option
+                .setName("amount")
+                .setRequired(true)
+                .setDescription("Number of messages to delete")
+        ),
     /**
      * 
      * @param {Discord.BaseCommandInteraction} interaction 
      */
     async execute(interaction){
-        const numDelete = interaction.options.getInteger("number");
-        const embed = new Discord.MessageEmbed();
 
-        await interaction.channel.bulkDelete(numDelete)
-        await interaction.reply({
-            embeds: [
-                embed.setAuthor({
+        try {
+            const numDelete = interaction.options.getInteger("amount");
+            const embed = new Discord.EmbedBuilder()
+                .setAuthor({
                     name: `${interaction.user.tag}`,
                     iconURL: interaction.user.avatarURL()
-                }).setTitle(`Bulk delete`).setDescription(`${numDelete} messages deleted`)
-                .setColor("LUMINOUS_VIVID_PINK")
-            ]
-        }).then(setTimeout(() => {
-            interaction.deleteReply()
-        }, 2000))
-    } 
+                })
+                .setTitle(`Bulk delete`)
+                .setDescription(`${numDelete} messages deleted`)
+                .setColor("LuminousVividPink")
+
+            await interaction.channel.bulkDelete(numDelete)
+
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: true
+            }).then(setTimeout(() => {
+                interaction.deleteReply()
+            }, 2000))
+
+        } catch(error){
+
+            const embed = new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: `${interaction.user.username}`,
+                    iconURL: interaction.user.avatarURL()
+                })
+                .setColor("LuminousVividPink")
+                .setDescription("Failed to delete messages")
+
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: true,
+            })
+
+            console.log(error)
+
+        }
+    }
 }
