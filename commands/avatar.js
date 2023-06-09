8ae53f1d8e93d57284b2a757a4cb8c040822ee60
Cyneric <cyneric00@gmail.com>
2023-06-09 18:27:18 +0800

    updated avatar command

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,48 +1,62 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
     
+const imageSizes = [16,32,64,128,256,512,1024,2048,4096]
+const choices = imageSizes.map((size)=>({
+    name: `${size}px`,
+    value: size
+}))
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("avatar")
         .setDescription("Returns avatar of user specified")
-        .addUserOption(option=>
-            option.setName("target")
-            .setDescription("Select a user"))
-        .addIntegerOption(option => 
-            option.setName("size")
-            .setDescription("Select size of the avatar")
-            .addChoices(
-                {name: "16",value: 16},
-                {name: "32",value: 32},
-                {name: "56",value: 56},
-                {name: "64",value: 64},
-                {name: "96",value: 96},
-                {name: "128",value: 128},
-                {name: "256",value: 256},
-                {name: "300",value: 300},
-                {name: "512",value: 512},
-                {name: "600",value: 600},
-                {name: "1024",value: 1024},
-                {name: "2048",value: 2048},
-                {name: "4096",value: 4096},
-            )),
+        .addUserOption((option)=>
+            option
+                .setName("target")
+                .setDescription("Select a user"))
+        .addIntegerOption((option)=>
+            option
+                .setName("size")
+                .setDescription("Select size of the avatar")
+                .addChoices(...choices)),
     /**
      * 
      * @param {Discord.BaseCommandInteraction} interaction 
      */
     async execute(interaction) {
-        const embed = new Discord.MessageEmbed();
-        const user = interaction.options.getUser("target");
-        const size = interaction.options.getInteger("size");
-        await interaction.reply({
-            embeds: [
-                embed.setAuthor({
-                    name: `${interaction.user.tag}`,
+        try {
+            const user = interaction.options.getUser("target");
+            const size = interaction.options.getInteger("size");
+            const embed = new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: `${interaction.user.username}`,
                     iconURL: interaction.user.avatarURL()
-                }).setTitle(`Avatar of ${user.tag}`).setImage(user.avatarURL({size: size}))
-                .setColor("LUMINOUS_VIVID_PINK")
-            ]
-        })
-        
+                })
+                .setTitle(`Avatar of ${user.username}`)
+                .setImage(user.avatarURL({size: size}))
+                .setColor("LuminousVividPink")
+
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: true,
+            })
+
+        } catch (error){
+            const embed = new Discord.EmbedBuilder()
+                .setAuthor({
+                    name: `${interaction.user.username}`,
+                    iconURL: interaction.user.avatarURL()
+                })
+                .setColor("LuminousVividPink")
+                .setDescription("Failed to get avatar of user")
+
+            await interaction.reply({
+                embeds: [embed],
+                ephemeral: true,
+            })
+
+            console.log(error)
+        }
     }
 }
